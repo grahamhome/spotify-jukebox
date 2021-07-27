7ff48afac55dc876fb59f2233922334b91d519fe
Graham Home <grahamhome333@gmail.com>
2021-07-27 18:40:24 -0400

    Initial commit - comments only

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    # Get # of Lifx lights N with Lifx API
+    ...
+    while True:
+        # Get current song from Spotify Player API
+        # If new song:
+        # Get "Now Playing" album art from Spotify Player API, download it
+        ...
+        # Get top N colors from album art via GCP Vision API
+        ...
+        # Set Lifx lights to album colors with Lifx API
+        # Set current song
+
+
+if __name__ == "__main__":
+    main()
